3ea5f79195919e0c8e917696ea06729537f48e31
Ayaz Alavi <ayaz@pursue.today>
2022-06-13 19:49:19 +0500

    Added card component

---
+++ components/Card.tsx
@@ -0,0 +1,38 @@
+import { useLayoutEffect, useRef, useState } from "react";
+import { VoteContext } from "../pages";
+import { Category } from "../types/ICategory";
+import { Nominee } from "../types/INominee";
+import styles from "./Card.module.css";
+
+interface Props {
+  nominee: Nominee;
+  onselect: (nominee: Nominee) => void;
+  selected: boolean;
+  category: Category;
+}
+
+const Card = (props: Props) => {
+  const { nominee, onselect, selected, category } = props;
+  const _onClick = () => onselect(nominee);
+
+  return (
+    <VoteContext.Consumer>
+      {({ votes, showpopup, addVote }) => (
+        <div className={selected ? styles.selected : styles.card}>
+          <span>{nominee.title}</span>
+          <img src={nominee.photoUrL} />
+          <button
+            onClick={() => {
+              addVote(nominee.id, category.id);
+              _onClick();
+            }}
+          >
+            Select for Vote
+          </button>
+        </div>
+      )}
+    </VoteContext.Consumer>
+  );
+};
+
+export default Card;
